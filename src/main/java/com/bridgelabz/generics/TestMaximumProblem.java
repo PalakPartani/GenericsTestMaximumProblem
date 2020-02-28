3544c7889e92b44a0ec1c5f622c09b8ccfde289a
PalakPartani <partanipalak16@gmail.com>
2020-02-28 16:49:15 +0530

    ADDED:method to find maximum of three float values

--- src/main/java/com/bridgelabz/generics/TestMaximumProblem.java
+++ src/main/java/com/bridgelabz/generics/TestMaximumProblem.java
@@ -3,13 +3,23 @@ package com.bridgelabz.generics;
 public class TestMaximumProblem {
 
     public Integer findMaximumNumber(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
-        Integer max = firstNumber;
-        if (secondNumber.compareTo(max)>0) {
-            max = secondNumber;
+        Integer maximumValue = firstNumber;
+        if (secondNumber.compareTo(maximumValue)>0) {
+            maximumValue = secondNumber;
         }
-        if(thirdNumber.compareTo(max)>0) {
-            max=thirdNumber;
+        if(thirdNumber.compareTo(maximumValue)>0) {
+            maximumValue=thirdNumber;
         }
-        return max;
+        return maximumValue;
+    }
+    public Float findMaximumNumber(Float firstNumber, Float secondNumber, Float thirdNumber) {
+        Float maximumValue = firstNumber;
+        if (secondNumber.compareTo(maximumValue)>0) {
+            maximumValue = secondNumber;
+        }
+        if(thirdNumber.compareTo(maximumValue)>0) {
+            maximumValue=thirdNumber;
+        }
+        return maximumValue;
     }
 }
--- src/test/java/com/bridgelabz/generics/TestMaximum.java
+++ src/test/java/com/bridgelabz/generics/TestMaximum.java
@@ -1,27 +1,49 @@
 package com.bridgelabz.generics;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestMaximum {
-    @Test
-    public void givenMaxNumberInIntegerAtFirstPosition_WhenProper_ShouldReturnTrue() {
-        TestMaximumProblem testMaximumProblem = new TestMaximumProblem();
-        int maximumNumber = testMaximumProblem.findMaximumNumber(5,3,1);
-        Assert.assertEquals(5,maximumNumber);
-        //System.out.println("Maximum Number is at first position :"+maxFind);
+    TestMaximumProblem testMaximumProblem;
+
+    @Before
+    public void setUp() throws Exception {
+        testMaximumProblem = new TestMaximumProblem();
     }
 
     @Test
-    public void givenMaxNumberAtSecondPositionInInteger_WhenProper_ShouldReturnTrue() {
-        TestMaximumProblem testMaximumProblem = new TestMaximumProblem();
+    public void givenMaxIntegerNumberAtFirstPosition_WhenProper_ShouldReturnTrue() {
+        int maximumNumber = testMaximumProblem.findMaximumNumber(5,3,1);
+        Assert.assertEquals(5,maximumNumber);
+    }
+
+    @Test
+    public void givenMaxIntegerNumberAtSecondPosition_WhenProper_ShouldReturnTrue() {
         int maximumNumber = testMaximumProblem.findMaximumNumber(3,5,1);
         Assert.assertEquals(5,maximumNumber);
     }
     @Test
-    public void givenMaxNumberAtThirdPositionInInteger_WhenProper_ShouldReturnTrue() {
-        TestMaximumProblem testMaximumProblem = new TestMaximumProblem();
+    public void givenMaxIntegerNumberAtThirdPosition_WhenProper_ShouldReturnTrue() {
         int maximumNumber = testMaximumProblem.findMaximumNumber(3,5,15);
         Assert.assertEquals(15,maximumNumber);
     }
+
+    @Test
+    public void givenMaxFloatNumberInIntegerAtFirstPosition_WhenProper_ShouldReturnTrue() {
+        Float maximumNumber = testMaximumProblem.findMaximumNumber(5.0f,3.0f,1.2f);
+        Assert.assertEquals(5,maximumNumber,0.0);
+    }
+    @Test
+    public void givenMaxFloatNumberAtSecondPosition_WhenProper_ShouldReturnTrue() {
+        Float maximumNumber = testMaximumProblem.findMaximumNumber(3.0f,5.0f,1.0f);
+        Assert.assertEquals(5,maximumNumber,0.0);
+    }
+    @Test
+    public void givenMaxFloatNumberAtThirdPosition_WhenProper_ShouldReturnTrue() {
+        Float maximumNumber = testMaximumProblem.findMaximumNumber(3.0f, 5.0f, 15.0f);
+        Assert.assertEquals(15, maximumNumber,0.0);
+    }
+
+
 }
