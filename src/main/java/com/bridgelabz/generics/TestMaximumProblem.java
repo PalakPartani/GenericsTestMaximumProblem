9229c8a2f3467955f3f35658efbeda446e2e51c4
PalakPartani <partanipalak16@gmail.com>
2020-02-29 11:06:51 +0530

    ADDED:extended method to take more than three arguements

--- src/main/java/com/bridgelabz/generics/TestMaximumProblem.java
+++ src/main/java/com/bridgelabz/generics/TestMaximumProblem.java
@@ -3,31 +3,39 @@ package com.bridgelabz.generics;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class TestMaximumProblem <E extends Comparable<E>>{
+public class TestMaximumProblem<E extends Comparable<E>> {
     private E firstValue;
     private E secondValue;
     private E thirdValue;
+    private E[] extraArguments;
 
-    public TestMaximumProblem(E firstValue, E secondValue, E thirdValue) {
+    public TestMaximumProblem(E firstValue, E secondValue, E thirdValue, E... extraArguements) {
         this.firstValue = firstValue;
         this.secondValue = secondValue;
         this.thirdValue = thirdValue;
+        this.extraArguments = extraArguements;
     }
-    public E findMaximum(){
-        E max=findMaximum(firstValue,secondValue,thirdValue);
+
+    public E findMaximum() {
+        E max = findMaximum(firstValue, secondValue, thirdValue, extraArguments);
         return max;
     }
 
-    public static <E extends Comparable<E>> E findMaximum(E firstNumber, E secondNumber, E thirdNumber) {
+    public static <E extends Comparable<E>> E findMaximum(E firstNumber, E secondNumber, E thirdNumber, E... extraArguments) {
 
         E maximumValue = firstNumber;
-        if (secondNumber.compareTo(maximumValue)>0) {
+        if (secondNumber.compareTo(maximumValue) > 0) {
             maximumValue = secondNumber;
         }
-        if(thirdNumber.compareTo(maximumValue)>0) {
-            maximumValue=thirdNumber;
+        if (thirdNumber.compareTo(maximumValue) > 0) {
+            maximumValue = thirdNumber;
         }
-
+        if (extraArguments.length != 0) {
+            Arrays.sort(extraArguments, Collections.reverseOrder());
+            if (extraArguments[0].compareTo(maximumValue) > 0) {
+                maximumValue = extraArguments[0];
+            }
+        }
         return maximumValue;
     }
 }
--- src/test/java/com/bridgelabz/generics/TestMaximum.java
+++ src/test/java/com/bridgelabz/generics/TestMaximum.java
@@ -5,54 +5,71 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestMaximum {
-    TestMaximumProblem testMaximumProblem;
 
     @Test
-    public void givenMaxIntegerNumberAtFirstPosition_WhenProper_ShouldReturnTrue() {
+    public void givenMaxIntegerNumberAtFirstPosition_WhenProper_ShouldReturnSameNumber() {
         Comparable maximumNumber = new TestMaximumProblem(5,3,2).findMaximum();
         Assert.assertEquals(5,maximumNumber);
     }
 
     @Test
-    public void givenMaxIntegerNumberAtSecondPosition_WhenProper_ShouldReturnTrue() {
-        Comparable maximumNumber = new TestMaximumProblem(3,5,1).findMaximum();
-        Assert.assertEquals(5,maximumNumber);
+    public void givenMaxIntegerNumberAtSecondPosition_WhenProper_ShouldReturnSameNumber() {
+        Comparable maximumNumber = new TestMaximumProblem(3,15,1).findMaximum();
+        Assert.assertEquals(15,maximumNumber);
     }
     @Test
-    public void givenMaxIntegerNumberAtThirdPosition_WhenProper_ShouldReturnTrue() {
+    public void givenMaxIntegerNumberAtThirdPosition_WhenProper_ShouldReturnSameNumber() {
         Comparable maximumNumber = new TestMaximumProblem(1,2,3).findMaximum();
         Assert.assertEquals(3,maximumNumber);
     }
     @Test
-    public void givenMaxFloatNumberAtFirstPosition_WhenProper_ShouldReturnTrue() {
+    public void givenMaxFloatNumberAtFirstPosition_WhenProper_ShouldReturnSameNumber() {
         Comparable maximumNumber = new TestMaximumProblem(5.5,2.2,1.0).findMaximum();
         Assert.assertEquals(5.5,maximumNumber);
     }
     @Test
-    public void givenMaxFloatNumberAtSecondPosition_WhenProper_ShouldReturnTrue() {
+    public void givenMaxFloatNumberAtSecondPosition_WhenProper_ShouldReturnSameNumber() {
         Comparable maximumNumber = new TestMaximumProblem(1.0,5.0,1.1).findMaximum();
         Assert.assertEquals(5.0,maximumNumber);
     }
     @Test
-    public void givenMaxFloatNumberAtThirdPosition_WhenProper_ShouldReturnTrue() {
+    public void givenMaxFloatNumberAtThirdPosition_WhenProper_ShouldReturnSameNumber() {
         Comparable maximumNumber = new TestMaximumProblem(1.0,2.3,6.0).findMaximum();
         Assert.assertEquals(6.0, maximumNumber);
     }
 
-
     @Test
-    public void givenMaxStringAtFirstPosition_WhenProper_ShouldReturnTrue() {
+    public void givenMaxStringAtFirstPosition_WhenProper_ShouldReturnSameString() {
         Comparable maximumString = new TestMaximumProblem("peach","apple","banana").findMaximum();
         Assert.assertEquals("peach",maximumString);
     }
+
     @Test
-    public void givenMaxStringAtSecondPosition_WhenProper_ShouldReturnTrue() {
+    public void givenMaxStringAtSecondPosition_WhenProper_ShouldReturnSameString() {
         Comparable maximumString = new TestMaximumProblem("apple","peach","banana").findMaximum();
         Assert.assertEquals("peach",maximumString);
     }
+
     @Test
-    public void givenMaxStringAtThirdPosition_WhenProper_ShouldReturnTrue() {
+    public void givenMaxStringAtThirdPosition_WhenProper_ShouldReturnSameString() {
         Comparable maximumString = new TestMaximumProblem("apple","banana","peach").findMaximum();
         Assert.assertEquals("peach", maximumString);
+    }
+
+    @Test
+    public void givenMaxIntegerNumberAtFirstPosition_WhenProperWithMultipleValues_ShouldReturnSameNumber() {
+        Comparable maximumNumber = new TestMaximumProblem(5,3,2,4).findMaximum();
+        Assert.assertEquals(5,maximumNumber);
+    }
+
+    @Test
+    public void givenMaxFloatNumberAtFirstPosition_WhenProperWithMultipleValues_ShouldReturnSameNumber() {
+        Comparable maximumNumber = new TestMaximumProblem(5.5,2.2,1.0,3.2).findMaximum();
+        Assert.assertEquals(5.5,maximumNumber);
+    }
+    @Test
+    public void givenMaxStringAtFirstPosition_WhenProperWithMultipleValues_ShouldReturnSameString() {
+        Comparable maximumString = new TestMaximumProblem("peach","apple","banana","orange").findMaximum();
+        Assert.assertEquals("peach",maximumString);
     }
 }
